3b6af3aaf9ac520e71c6a09d92ebb13ef529f577
GuillermoMartin <guillemsilgadod@gmail.com>
2025-10-24 11:28:06 +0200

    Fallos corregidos de gradle y conexión del xml al main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.guillermoMartin.calculadoraviaje"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.guillermoMartin.calculadoraviaje"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
