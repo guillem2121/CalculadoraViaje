be21f178e69df09cc1f52c6d09036d69ce09c735
GuillermoMartin <guillemsilgadod@gmail.com>
2025-10-24 13:21:06 +0200

    Creación de la nueva activity para la calculadora.

---
+++ app/src/main/java/com/guillermoMartin/calculadoraviaje/CaulculatorActivity.kt
@@ -0,0 +1,20 @@
+package com.guillermoMartin.calculadoraviaje
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CaulculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_caulculator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
